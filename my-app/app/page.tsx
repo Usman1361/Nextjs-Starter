7aa8d734d6ca373ba7dcfc6df397aa39ad38df70
Muhammad Usman <112246337+Usman1361@users.noreply.github.com>
2023-07-09 12:36:14 +0500

    Next Js Starter Folder Structure Commit

---
+++ my-app/app/page.tsx
@@ -0,0 +1,14 @@
+"use client";
+
+import Footer from "./(Components)/Footer/Footer";
+import Header from "./(Components)/Header/Header";
+
+export default function Home() {
+  return (
+    <>
+      Hi
+      <Header />
+      <Footer />
+    </>
+  );
+}
